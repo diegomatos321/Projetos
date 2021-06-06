a86b45e97a9febb27366b148ef342ac7e9887a22
Diego Matos <59349414+diegomatos321@users.noreply.github.com>
2021-06-05 23:46:15 -0300

    Implementado State Machine com Animation Tree
    Ainda não funciona muito bem

--- Godot/A Night In Gothan City/scripts/player.gd
+++ Godot/A Night In Gothan City/scripts/player.gd
@@ -1,14 +1,9 @@
 extends KinematicBody2D
 
-enum PlayerStates {
-	IDLE,
-	MOVE,
-	RUN,
-	JUMP,
-	CROUNCH
-};
+onready var body = get_node("Body")
+onready var animationTree = get_node("AnimationTree")
+onready var animation_mode = animationTree.get("parameters/playback")
 
-var currentState = PlayerStates.IDLE;
 var velocityVector = Vector2()
 
 export (int) var speed # pixels / sec
@@ -20,51 +15,35 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(dt):
+	pass
+
+func _physics_process(dt):
 	handlePlayerMovement(dt)
-	handlePlayerAnimation()
-	
 	pass
 
 func handlePlayerMovement(dt):
-	currentState = PlayerStates.IDLE
 	velocityVector.x = 0
 	velocityVector.y += gravity * dt
 	
 	if Input.is_action_pressed("ui_right"):
-		velocityVector.x = cos(Vector2.RIGHT.angle()) * speed
-		get_node("AnimatedSprite").flip_h = false
-		currentState = PlayerStates.MOVE
+		velocityVector.x = speed
+		body.flip_h = false
+		animation_mode.travel("run")
 	elif Input.is_action_pressed("ui_left"):
-		velocityVector.x = cos(Vector2.LEFT.angle()) * speed
-		get_node("AnimatedSprite").flip_h = true
-		currentState = PlayerStates.MOVE
+		velocityVector.x = -speed
+		body.flip_h = true
+		animation_mode.travel("run")
+		#currentState = PlayerStates.MOVE
 	else:
 		velocityVector.x = 0
+		animation_mode.travel("idle")
 	
 	if is_on_floor():
 		velocityVector.y = 0
 		
 		if Input.is_action_just_pressed("ui_up"):
-			velocityVector.y = sin(Vector2.UP.angle()) * jump_force
-			currentState = PlayerStates.JUMP
+			velocityVector.y = -jump_force
+			#currentState = PlayerStates.JUMP
 	
 	move_and_slide(velocityVector, Vector2(0, -1));
 	pass
-
-func handlePlayerAnimation():
-	if currentState == PlayerStates.IDLE:
-		get_node("AnimatedSprite").play("idle")
-	
-	if currentState == PlayerStates.MOVE:
-		get_node("AnimatedSprite").play("start_run")
-	
-	pass
-
-
-func _on_AnimatedSprite_animation_finished():
-	print(get_node("AnimatedSprite").animation)
-	
-	if get_node("AnimatedSprite").animation == "start_run":
-		print("Start Run animation")
-		get_node("AnimatedSprite").play("run")
-	pass 
