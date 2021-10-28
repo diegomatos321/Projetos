db0744d433d2ebab1d9ba6218b1f4d920c2f95aa
Diego Matos <diegovasconcelos62@gmail.com>
2021-10-28 20:14:22 -0300

    Revisando app

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path")
 const mongoose = require("mongoose");
 
-const flappybirdRouter = require("./js/routes/flappybirdRouter.js")
-const bodyParser = require("body-parser")
-require("dotenv/config")
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname)));
 
-app.use(bodyParser.json())
-
-let htmlPath = path.join(__dirname);
-app.use(express.static(htmlPath));
-
+//Routes
+const flappybirdRouter = require("./js/routes/flappybirdRouter.js");
 app.use("/", flappybirdRouter);
 
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
-
 mongoose.connection.on('error', error => console.log("Ocorreu um erro ao conectar na base de dados: ", error));
 mongoose.connection.once('open', () => console.log("Conectado na base de dados"));
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Servidor Ligado !")
+  console.log("Servidor Ligado !")
 });
