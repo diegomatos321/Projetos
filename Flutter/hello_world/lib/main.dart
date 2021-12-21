0c877cf02c392b729df7a874fe6511e759a65317
diegomatos <diegovasconcelos62@gmail.com>
2021-12-21 18:59:57 -0300

    Treinando com Rows e Containers

--- Flutter/hello_world/lib/main.dart
+++ Flutter/hello_world/lib/main.dart
@@ -18,30 +18,61 @@ class HelloWorld extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Minha primeira Aplicação em Flutter")
       ),
-      body: Center(
-        child: Container(
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey,
-                blurRadius: 10
-              )
-            ]
-          ),
-          alignment: Alignment.center,
-          child: const Text(
-            "Hello World",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 24,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
+      body: Container(
+        width: 500,
+        height: 500,
+        color: Colors.black,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 10
+                  )]
+              ),
+              alignment: Alignment.center,
+              child: const Text(
+                "Hello World",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                )
+              ),
             ),
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 10
+                  )]
+              ),
+              alignment: Alignment.center,
+              child: const Text(
+                "Hello World",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                )
+              ),
             ),
+          ],
         ),
       ),
     );
