59008b2ddfbafab20f92e3e69d32b25198716c98
diegomatos <diegovasconcelos62@gmail.com>
2021-12-22 11:34:34 -0300

    Aprendendo básico do formulário

--- PhP/hello_world/index.php
+++ PhP/hello_world/index.php
@@ -10,11 +10,29 @@
   <?php
     $nome = "Tom";
     $idade = 2;
+    if($_POST) {
+      $nome = $_POST["nome"];
+      $idade = $_POST["idade"];
+    }
+
     $fofura = 10.0;
     $isMale = true;
 
-    echo "<h1>Tipos de Variáveis</h1>";
+    echo "<h1>Formulários</h1>";
     echo "<p>$nome tem $idade anos e é uma fofura nota $fofura</p>";
   ?>
+
+  <form action="/hello_world/index.php" method="POST">
+    <fieldset>
+      <legend>Digite nos campos para alterar o texto</legend>
+      <label for="nome">Nome:</label>
+      <input type="text" id="nome" name="nome"/>
+
+      <label for="idade">Idade:</label>
+      <input type="text" id="idade" name="idade"/>
+
+      <input type="submit" value="Enviar">
+    </fieldset>
+  </form>
 </body>
 </html>
