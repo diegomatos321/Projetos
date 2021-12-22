0abbdb635c8c96c960a4b1ea8659aecc9571119a
diegomatos <diegovasconcelos62@gmail.com>
2021-12-22 18:13:46 -0300

    Trabalhando com checkbox

--- PhP/hello_world/index.php
+++ PhP/hello_world/index.php
@@ -10,9 +10,11 @@
   <?php
     $nome = "Tom";
     $idade = 2;
+    $comidas_favoritas = array();
     if($_POST) {
       $nome = $_POST["nome"];
       $idade = $_POST["idade"];
+      $comidas_favoritas = $_POST["comidas"];
     }
 
     $fofura = 10.0;
@@ -20,6 +22,17 @@
 
     echo "<h1>Formulários</h1>";
     echo "<p>$nome tem $idade anos e é uma fofura nota $fofura</p>";
+
+    if(count($comidas_favoritas) > 0) {
+      echo "<p>Ele adora comer</p>";
+      echo "<ul>";
+
+      for ($i = 0; $i < count($comidas_favoritas); $i++) { 
+        echo "<li>$comidas_favoritas[$i]</li>";
+      }
+
+      echo "</ul>";
+    }
   ?>
 
   <form action="/hello_world/index.php" method="POST">
@@ -30,6 +43,13 @@
 
       <label for="idade">Idade:</label>
       <input type="text" id="idade" name="idade"/>
+
+      <label for="melao">Melão</label>
+      <input type="checkbox" name="comidas[]" id="melao" value="melao"/>
+      <label for="maca">Maçã</label>
+      <input type="checkbox" name="comidas[]" id="maca" value="maçã"/>
+      <label for="pera">Pêra</label>
+      <input type="checkbox" name="comidas[]" id="pera" value="pêra"/>
 
       <input type="submit" value="Enviar">
     </fieldset>
