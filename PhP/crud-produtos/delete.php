7b946b044fc1ca77c3d61d48cb5dac90c8c4fb1e
diegomatos <diegovasconcelos62@gmail.com>
2021-12-24 12:24:09 -0300

    Finalizando exercício

--- PhP/crud-produtos/delete.php
+++ PhP/crud-produtos/delete.php
@@ -4,6 +4,17 @@ require_once "database_connection.php";
 
 $id = $_GET["id"];
 
+$sql = "SELECT * FROM produtos WHERE id = :id";
+$statement = $pdo->prepare($sql);
+$statement->bindValue(":id", $id);
+$statement->execute();
+$produto = $statement->fetch(PDO::FETCH_ASSOC);
+
+$current_image = $produto["imagem"];
+if ($current_image && !empty($current_image)) {
+  unlink($current_image);
+}
+
 $sql = "DELETE FROM produtos WHERE id = :id";
 $statement = $pdo->prepare($sql);
 $statement->bindValue(":id", $id);
--- PhP/crud-produtos/index.php
+++ PhP/crud-produtos/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt_BR">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,49 +14,72 @@
   </style>
 
   <?php
-    require_once "database_connection.php";
+  require_once "database_connection.php";
 
+  $search = $_GET["search"] ?? null;
+
+  if ($search && !empty($search)) {
+    $sql = "SELECT * FROM produtos WHERE nome LIKE :search ORDER BY data_criacao DESC";
+    $statement = $pdo->prepare($sql);
+    $statement->bindValue(":search", "%$search%");
+  } else {
     $sql = "SELECT * FROM produtos ORDER BY data_criacao DESC";
     $statement = $pdo->prepare($sql);
-    $statement->execute();
+  }
+  
+  $statement->execute();
 
-    $produtos = $statement->fetchAll(PDO::FETCH_ASSOC);
+  $produtos = $statement->fetchAll(PDO::FETCH_ASSOC);
   ?>
 </head>
+
 <body>
-  <h1>Gerencie seus produtos</h1>
-  <?php require_once "create_form.php" ?>
-  <table class="table table-hover">
-    <thead>
-      <tr>
-        <th scope="col">id</th>
-        <th scope="col">Nome Produto</th>
-        <th scope="col">Descrição</th>
-        <th scope="col">Preço</th>
-        <th scope="col">Data de Criação</th>
-        <th scope="col"></th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach ($produtos as $produto): ?>
+  <main>
+    <header>
+      <h1>Gerencie seus produtos</h1>
+      <a class="btn btn-success" href="create.php">Criar Produto</a>
+    </header>
+    <br/>
+    <form action="" method="GET">
+      <div class="input-group mb-3">
+        <input type="text" name="search" id="search" class="form-control" placeholder="Busque por produtos" value="<?php echo $search?>"/>
+        <button type="submit" class="btn btn-primary">Buscar</button>
+      </div>
+    </form>
+    <table class="table table-hover">
+      <thead>
         <tr>
-          <th scope="row">
-            <?php echo $produto["id"] ?>
-            <?php if ($produto["imagem"] && !empty($produto["imagem"])): ?>
-              <img src=<?php echo $produto["imagem"] ?> alt=<?php echo $produto["imagem"] ?> width=100/>
-            <?php endif;?>
-          </th>
-          <td><?php echo $produto["nome"] ?></td>
-          <td><?php echo $produto["descricao"] ?></td>
-          <td><?php echo $produto["preco"] ?></td>
-          <td><?php echo $produto["data_criacao"] ?></td>
-          <td>
-            <a href="update.php?id=<?php echo $produto["id"]?>" class="btn btn-info">Editar</a>
-            <a href="delete.php?id=<?php echo $produto["id"]?>" class="btn btn-danger">Deletar</a>
-          </td>
+          <th scope="col">id</th>
+          <th scope="col">Nome Produto</th>
+          <th scope="col">Descrição</th>
+          <th scope="col">Preço</th>
+          <th scope="col">Data de Criação</th>
+          <th scope="col"></th>
         </tr>
-      <?php endforeach; ?>
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        <?php foreach ($produtos as $produto) : ?>
+          <tr>
+            <th scope="row">
+              <?php echo $produto["id"] ?>
+              <?php if ($produto["imagem"] && !empty($produto["imagem"])) : ?>
+                <img src=<?php echo $produto["imagem"] ?> alt=<?php echo $produto["imagem"] ?> width=100 />
+              <?php endif; ?>
+            </th>
+            <td><?php echo $produto["nome"] ?></td>
+            <td><?php echo $produto["descricao"] ?></td>
+            <td>R$ <?php echo $produto["preco"] ?></td>
+            <td><?php echo $produto["data_criacao"] ?></td>
+            <td>
+              <a href="update.php?id=<?php echo $produto["id"] ?>" class="btn btn-info">Editar</a>
+              <a href="delete.php?id=<?php echo $produto["id"] ?>" class="btn btn-danger">Deletar</a>
+            </td>
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+
+  </main>
 </body>
+
 </html>
