545ab583b72b3a302a3a1b85f420f7665a0f1a70
diegomatos <diegovasconcelos62@gmail.com>
2021-12-29 14:54:41 -0300

    composer, autoloading, namespaces

---
+++ PhP/autoloading/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4ee705ff17364a0e5e98c9cfa8a19101d2c33ef1',
+        'name' => 'diego/autoloading',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'diego/autoloading' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4ee705ff17364a0e5e98c9cfa8a19101d2c33ef1',
+            'dev_requirement' => false,
+        ),
+    ),
+);
