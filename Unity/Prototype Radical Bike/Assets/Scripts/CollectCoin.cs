1b5028c6c5b5f2a9b3ed437f642318586573f3e8
diegomatos <diegovasconcelos62@gmail.com>
2022-03-20 13:33:13 -0300

    Aplicando fade out na moeda ao ser coletada

--- Unity/Prototype Radical Bike/Assets/Scripts/CollectCoin.cs
+++ Unity/Prototype Radical Bike/Assets/Scripts/CollectCoin.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class CollectCoin : MonoBehaviour
 {
+    private Animator animator;
+    private CircleCollider2D collider;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+        collider = GetComponent<CircleCollider2D>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log(other.tag);
         if (other.CompareTag("Player"))
         {
-            Destroy(this.gameObject);
+            // Destroy(this.gameObject);
+            collider.enabled = false;
+            animator.SetBool("finish", true);
         }
+    }
+
+    private void DestroyObject() {
+        Destroy(this.gameObject);
     }
 }
