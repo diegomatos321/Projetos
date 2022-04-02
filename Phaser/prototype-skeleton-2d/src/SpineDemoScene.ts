7dd88483f6b63ce770711506bbed61e7637fd10a
diegomatos <diegovasconcelos62@gmail.com>
2022-04-02 14:09:06 -0300

    Carregando fonte bitmap

--- Phaser/prototype-skeleton-2d/src/SpineDemoScene.ts
+++ Phaser/prototype-skeleton-2d/src/SpineDemoScene.ts
@@ -1,14 +1,26 @@
 export default class SpineDemoScene extends Phaser.Scene {
+  private GAME_WIDTH: number;
+  private GAME_HEIGHT: number;
+
+  private Player: SpineGameObject;
+
   constructor() {
     super({ key: "SpineDemo "});
   }
 
+  init = () => {
+    this.GAME_WIDTH = Number(this.game.config.width);
+    this.GAME_HEIGHT = Number(this.game.config.width);
+  }
+
   private preload = (): void => {
     this.load.setPath("./");
-    this.load.spine("spineboy", "spineboy.json", "spineboy.atlas");
+    this.load.spine("spineboy", "./assets/spine/spineboy.json", "./assets/spine/spineboy.atlas");
+    this.load.bitmapFont("typewrite", "./assets/fonts/mainfont_0.png", "./assets/fonts/mainfont.xml") 
   }
 
   private create = (): void => {
-    this.add.spine(400, 600, "spineboy", "idle", true);
+    this.Player = this.add.spine(400, 600, "spineboy", "idle", true).setScale(0.5);
+    this.add.bitmapText(this.GAME_WIDTH / 2, 20, "typewrite", "Teste de Fonte").setOrigin(0.5);
   }
 }
