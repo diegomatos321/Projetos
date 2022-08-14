2280f43e3ed6319e1e532dce4c8bf1b62626372c
diegomatos321 <diegovsm@dcc.ufrj.br>
2022-08-14 14:53:15 -0300

    Colunas e Linhas

--- Flutter/app_with_game/lib/main.dart
+++ Flutter/app_with_game/lib/main.dart
@@ -13,12 +13,43 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Hello World'),
-      ),
-      body: Container(
-        child: Text('Hi Flutter'),
-      )
-    );
+        appBar: AppBar(
+          title: Text('Hello World'),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(8),
+              color: Colors.red,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    child: Text('Conheça mais sobre pokemons'),
+                  ),
+                  Container(
+                    child: Text('Eles são muito legais')
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(8),
+              color: Colors.yellow,
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    child: Text('Conheça mais sobre pokemons'),
+                  ),
+                  Container(
+                    child: Text('Eles são muito legais')
+                  )
+                ],
+              ),
+            )
+          ],
+        )
+      );
   }
 }
