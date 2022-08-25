0279e9bd5c722ef3feb86e0ad7f0ffa9b5c787e6
diegomatos <diegovasconcelos62@gmail.com>
2022-08-25 20:26:26 -0300

    Embutindo jogo ao app

--- Flutter/app_with_game/lib/Pages/JogosPage/jogos_page.dart
+++ Flutter/app_with_game/lib/Pages/JogosPage/jogos_page.dart
@@ -1,10 +1,23 @@
 import 'package:app_with_game/Widgets/sign_out_btn.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_unity_widget/flutter_unity_widget.dart';
 
-class JogosPage extends StatelessWidget {
+class JogosPage extends StatefulWidget {
   static const routeName = 'JogosPage';
 
   const JogosPage({Key? key}) : super(key: key);
+
+  @override
+  State<JogosPage> createState() => _JogosPageState();
+}
+
+class _JogosPageState extends State<JogosPage> {
+  static final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  late UnityWidgetController _unityWidgetController;
+
+  void onUnityCreated(controller) {
+    this._unityWidgetController = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +28,13 @@ class JogosPage extends StatelessWidget {
           SignOutBtn()
         ],
       ),
-      body: Column(),
+      body: SafeArea(
+        bottom: false,
+        child: UnityWidget(
+          onUnityCreated: onUnityCreated,
+          useAndroidViewSurface: true,
+        ),
+      ),
     );
   }
 }
