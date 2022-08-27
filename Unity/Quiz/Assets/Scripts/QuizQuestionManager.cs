a721c567e40709a06404b3be6d0726b2b8fab2c4
diegomatos <diegovasconcelos62@gmail.com>
2022-08-27 16:42:06 -0300

    Lógica para desabilitar os botões e mostrar proxima questão

--- Unity/Quiz/Assets/Scripts/QuizQuestionManager.cs
+++ Unity/Quiz/Assets/Scripts/QuizQuestionManager.cs
@@ -7,12 +7,17 @@ using UnityEngine.UI;
 public class QuizQuestionManager : MonoBehaviour
 {
     [SerializeField] protected QuestionTemplate questionData;
-    [SerializeField] TextMeshProUGUI questionTitle;
-    [SerializeField] GameObject[] answersButtons;
-    [SerializeField] Sprite correctAnswerSprite;
-    [SerializeField] Sprite wrongAnswerSprite;
+    [SerializeField] protected TextMeshProUGUI questionTitle;
+    [SerializeField] protected GameObject[] answersButtons;
+    [SerializeField] protected Sprite correctAnswerSprite;
+    [SerializeField] protected Sprite defaultAnswerSprite;
 
-    private void Start() 
+    private void Start()
+    {
+        this.DisplayQuestion();
+    }
+
+    private void DisplayQuestion()
     {
         this.questionTitle.text = this.questionData.GetQuestion();
 
@@ -25,7 +30,8 @@ public class QuizQuestionManager : MonoBehaviour
         }
     }
 
-    public void OnAnswerSelected(int index) {
+    public void OnAnswerSelected(int index) 
+    {
 
         if (index == this.questionData.GetCorrectAnswerIndex())
         {
@@ -37,6 +43,30 @@ public class QuizQuestionManager : MonoBehaviour
             this.answersButtons[correctAnswerIndex].GetComponent<Image>().sprite = this.correctAnswerSprite;
             string correctAnswerText = this.questionData.GetAnswer(correctAnswerIndex);
             questionTitle.text = "Que pena, você errou, a resposta correta é:\n" + correctAnswerText;
+        }
+
+        this.SetAnswerInteractable(false);
+    }
+
+    protected void SetAnswerInteractable(bool newState) 
+    {
+        foreach (GameObject answerButton in this.answersButtons)
+        {
+            answerButton.GetComponent<Button>().interactable = newState;
+        }
+    }
+
+    protected void NextQuestion() 
+    {
+        this.DisplayQuestion();
+        this.SetAnswerInteractable(true);
+    }
+
+    protected void SetDefaultAnswerSprite()
+    {
+        foreach (GameObject answerButton in this.answersButtons)
+        {
+            answerButton.GetComponent<Image>().sprite = this.defaultAnswerSprite;
         }
     }
 }
