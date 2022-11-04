3d46a7e2fd99917b235bc82ef8eff83161c49217
diegomatos <diegovasconcelos62@gmail.com>
2022-11-04 10:40:35 -0300

    Extraindo para Stateful Widget

--- Flutter/random_english_words/lib/main.dart
+++ Flutter/random_english_words/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
@@ -10,18 +12,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-
     return MaterialApp(
       title: 'Random English Words',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Random English Words'),
         ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
+        body: const Center(
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+
+    return Text(wordPair.asPascalCase);
   }
 }
