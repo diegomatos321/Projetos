37835709c2d8465c9ecf6d09b082794ce8ab8658
diegomatos <diegovasconcelos62@gmail.com>
2022-11-04 10:34:10 -0300

    Primeira palavra randomica

--- Flutter/random_english_words/lib/main.dart
+++ Flutter/random_english_words/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,14 +10,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+
     return MaterialApp(
       title: 'Random English Words',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Random English Words'),
         ),
-        body: const Center(
-          child: Text('Olá Mundo!'),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
