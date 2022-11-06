b057f0b4ad6ef73b73ddc209e5d71418b91ac9cc
diegomatos <diegovasconcelos62@gmail.com>
2022-11-06 11:09:43 -0300

    Pickup Coin with SFX

--- Unity/TileVania/Assets/Scripts/CoinPickUp.cs
+++ Unity/TileVania/Assets/Scripts/CoinPickUp.cs
@@ -4,32 +4,21 @@ using UnityEngine;
 
 public class CoinPickUp : MonoBehaviour
 {
+    [SerializeField] private AudioClip coinSfx;
     private Animator animator;
-    private AudioSource coinSfx;
 
     // Start is called before the first frame update
     void Start()
     {
-        this.coinSfx = this.GetComponent<AudioSource>();
         this.animator = this.GetComponent<Animator>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log(other.tag);
         if (other.tag != "Player")
             return;
-        
-        
-    }
-
-    void PlaySfx()
-    {
-        this.coinSfx.Play();
-    }
-
-    void DestroyCoin()
-    {
+    
+        AudioSource.PlayClipAtPoint(this.coinSfx, Camera.main.transform.position);
         Destroy(this.gameObject);
     }
 }
