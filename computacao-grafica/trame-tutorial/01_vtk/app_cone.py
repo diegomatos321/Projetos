789fde6b55da66f3353cb5aa5e3182f50f2f69ec
diegomatos <diegovasconcelos62@gmail.com>
2024-01-11 20:01:31 -0300

    Visualizando cone

--- computacao-grafica/trame-tutorial/01_vtk/app_cone.py
+++ computacao-grafica/trame-tutorial/01_vtk/app_cone.py
@@ -1,5 +1,16 @@
 from trame.app import get_server
 from trame.ui.vuetify import SinglePageLayout
+from trame.widgets import vtk, vuetify
+from vtkmodules.vtkFiltersSources import vtkConeSource
+from vtkmodules.vtkRenderingCore import (
+    vtkActor,
+    vtkPolyDataMapper,
+    vtkRenderer,
+    vtkRenderWindow,
+    vtkRenderWindowInteractor,
+)
+from vtkmodules.vtkInteractionStyle import vtkInteractorStyleSwitch #noqa
+import vtkmodules.vtkRenderingOpenGL2 #noqa
 
 # -----------------------------------------------------------------------------
 # Get a server to work with
@@ -11,8 +22,33 @@ server = get_server(client_type = "vue2")
 # GUI
 # -----------------------------------------------------------------------------
 
+renderer = vtkRenderer()
+renderWindow = vtkRenderWindow()
+renderWindow.AddRenderer(renderer)
+
+renderWindowInteractor = vtkRenderWindowInteractor()
+renderWindowInteractor.SetRenderWindow(renderWindow)
+renderWindowInteractor.GetInteractorStyle().SetCurrentStyleToTrackballCamera()
+
+cone_source = vtkConeSource()
+mapper = vtkPolyDataMapper()
+mapper.SetInputConnection(cone_source.GetOutputPort())
+actor = vtkActor()
+actor.SetMapper(mapper)
+
+renderer.AddActor(actor)
+renderer.ResetCamera()
+
 with SinglePageLayout(server) as layout:
     layout.title.set_text("Hello trame")
+
+    with layout.content:
+        with vuetify.VContainer(
+            fluid=True,
+            classes="pa-0 fill-height"
+        ):
+            html_view = vtk.VtkLocalView(renderWindow)
+            server.controller.on_server_ready.add(html_view.update)
 
 # -----------------------------------------------------------------------------
 # Main
