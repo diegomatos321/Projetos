02effc55e6d10f0900774b8abf87f421f373a359
diegomatos <diegovasconcelos62@gmail.com>
2024-08-27 16:59:47 -0300

    WIP: Plugin development hello world

---
+++ wordpress/dummy-site/wp-content/plugins/multi-columns/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a8bdd5b17f958aa9d7e6');
---
+++ wordpress/dummy-site/wp-content/plugins/multi-columns/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '9d1b86f0981f8742499b');
