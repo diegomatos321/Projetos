1dc2908630ca492debee60669f289224657e91ee
diegomatos <diegovasconcelos62@gmail.com>
2024-08-28 22:28:36 -0300

    Removendo diretorio Build

--- wordpress/dummy-site/wp-content/plugins/multi-columns/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '7175f2a77c31e83f58cd');
--- wordpress/dummy-site/wp-content/plugins/multi-columns/build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '9d1b86f0981f8742499b');
