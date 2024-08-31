4335f2cf1d5a55faa376a74838262765a5f977fe
diegomatos <diegovasconcelos62@gmail.com>
2024-08-31 19:42:29 -0300

    CG: Exercicio forma misteriosa

---
+++ computacao-grafica/exercicio-mystery-shape-shadertoy-style/main.ts
@@ -0,0 +1,328 @@
+let shapeValue = 'box'
+let coloringValue = 'plainColor'
+let offsetValue = 0
+
+const shadersOptions = {
+    vertex: `
+        attribute vec4 position;
+        void main() {
+            gl_Position = position; // Vertex position
+        }
+    `,
+    circle: `
+        // Distance from p to a circle of radius 0.7
+        float signedDistance (in vec2 p) {
+            return sdCircle (p, 0.7);
+        }
+    `,
+    box: `
+        // Distance from p to a box 1.5 units wide and 1 units high
+        float signedDistance (in vec2 p) {
+            return sdBox(p, vec2(0.75, 0.5));
+        }
+    `,
+    boxAndCircle: `
+        // Distance from p to the intersection between a box 1.5 units wide and 1 units high
+        // and a circle of radius 0.7
+        float signedDistance (in vec2 p) {
+            return max(sdBox(p, vec2(0.75, 0.5)), sdCircle(p,0.7));
+        }
+    `,
+    boxOrCircle: `
+        // Distance from p to the union between a box 1.5 units wide and 1 units high
+        // and a circle of radius 0.7
+        float signedDistance (in vec2 p) {
+            return min(sdBox(p, vec2(0.75, 0.5)), sdCircle(p,0.7));
+        }
+    `,
+    boxMinusCircle: `
+        // Distance from p to the difference between a box 1.5 units wide and 1 units high
+        // and a circle of radius 0.7
+        float signedDistance (in vec2 p) {
+            return max(sdBox(p, vec2(0.75, 0.5)), -sdCircle(p,0.7));
+        }
+    `,
+    circleMinusBox: `
+        // Distance from p to the difference between a circle of radius 0.7
+        // and a box 1.5 units wide and 1 units high
+        float signedDistance (in vec2 p) {
+            return max(-sdBox(p, vec2(0.75, 0.5)), sdCircle(p,0.7));
+        }
+    `,
+    plainColoring: `
+        // Simple coloring scheme for distance d: white inside
+        // and orange outside
+        vec3 color (in float d) {
+            return vec3(1.0) - sign(d)*vec3(0.1,0.4,0.7);
+        }
+    `,
+    mystery: `
+        float roundedLineShape (in vec2 p1, in vec2 p2, in vec2 p3) {
+            float thickness = 0.2;
+            vec2 line = p2 - p1;
+            
+            vec2 proj = normalize(line) * dot(p3, normalize(line));
+
+            float circleP1 = sdCircle(p3 + p1, thickness);
+            float circleP2 = sdCircle(p3 + p2, thickness);
+
+            // Clamp the line to a segment line with Thickness
+            if (length(proj - p1) <= length(line) && length(proj - p2) <= length(line)) {
+                float clampedLineWithThickness = length(p3 - proj) - thickness;
+
+                return clampedLineWithThickness;
+            }
+
+            return min(circleP1, circleP2);
+        }
+
+        float ringShape (in vec2 p) {
+            float outerRadius = 0.7;
+            float innerRadius = 0.6;
+
+            float distToOuter = sdCircle(p, outerRadius);
+            float distToInner = sdCircle(p, innerRadius);
+
+            return max(distToOuter, -distToInner);
+        }
+
+        float signedDistance(in vec2 p) {
+            float segment1 = roundedLineShape(vec2(-0.5, 0.5), vec2(0.5, -0.5), p);
+            float segment2 = roundedLineShape(vec2(0.5, 0.5), vec2(-0.5, -0.5), p);
+            float cross = min(segment1, segment2);
+
+            return max(cross, -ringShape(p));
+        }
+    `,
+    iqColoring: `
+        // Inigo Quilez's coloring scheme for a distance function. Draws isolines
+        // and highlights the region close to the border. Nice!
+        vec3 color (in float d) {
+            vec3 col = vec3(1.0) - sign(d)*vec3(0.1,0.4,0.7);
+            col *= 1.0 - exp(-3.0*abs(d));
+            float c = cos(150.0*d);
+            col *= 0.8 + 0.2*c*c*c;
+            col = mix( col, vec3(1.0), 1.0-smoothstep(0.0,0.01,abs(d)) );
+            return col;
+        }
+    `,
+    fragment: function (distanceFunc, colorFunc) {
+        return `
+            precision mediump float;
+            uniform vec3 iResolution; // Size of the viewport in pixels
+            uniform float distanceOffset; // Offset value to be added to distance function
+        
+            //
+            // Signed distance from point 'p' to a circle of radius 'r' centered at the origin 
+            //
+            float sdCircle( in vec2 p, in float r )   
+            {
+                return length(p)-r;
+            }
+        
+            //
+            // Signed distance from point 'p' to an axes-aligned rectangle centered at the origin and 
+            // having width and height equal to b.x/2 and b.y/2, respectively.
+            //
+            float sdBox( in vec2 p, in vec2 b )
+            {
+                vec2 d = abs(p)-b;
+                return length(max(d,0.0)) + min(max(d.x,d.y),0.0);
+            }
+
+            ${distanceFunc}
+            ${colorFunc}
+
+            void main() {
+                // Translate coordinates to center of screen and normalize to range [-1..1]
+                vec2 p = (2.0*gl_FragCoord.xy-iResolution.xy)/iResolution.y;
+            
+                // Compute signed distance to the shape and offset it
+                float d = signedDistance(p) + distanceOffset;
+            
+                // Coloring
+                vec3 col = color(d);
+            
+                gl_FragColor = vec4(col,1.0); 
+            }
+      `}
+}
+
+document.addEventListener("DOMContentLoaded", main);
+
+function main() {
+    AttachToInputElements()
+    Render()
+}
+
+function Render() {
+    const canvas = document.getElementById("canvas") as HTMLCanvasElement | null
+    if (canvas === null) {
+        throw new Error("Canvas elements not found")
+    }
+
+    const gl: WebGLRenderingContext | null = canvas.getContext("webgl");
+    if (gl === null) {
+        throw new Error("Webgl not supported, try of latest browser");
+    }
+
+    const shaderProgram = initShaderProgram(gl, shadersOptions.vertex, shadersOptions.fragment(shadersOptions[shapeValue], shadersOptions[coloringValue]))
+    const buffers = initBuffers(gl)
+
+    const programInfo = {
+        program: shaderProgram,
+        attribLocations: {
+            vertexPosition: gl.getAttribLocation(shaderProgram, "position"),
+        },
+        uniformLocations: {
+            resolution: gl.getUniformLocation(shaderProgram, "iResolution"),
+            distanceOffset: gl.getUniformLocation(shaderProgram, "distanceOffset")
+        }
+    }
+
+    drawScene(gl, programInfo, buffers)
+}
+
+function AttachToInputElements() {
+    const shapeInput = document.getElementById("shape") as HTMLSelectElement | null
+    if (shapeInput !== null) {
+        shapeValue = shapeInput.value
+        shapeInput.addEventListener("change", (e: Event) => {
+            const target = e.target as HTMLSelectElement
+            shapeValue = target.value
+            Render()
+        })
+    }
+
+    const coloringInput = document.getElementById("coloring") as HTMLSelectElement | null
+    if (coloringInput !== null) {
+        coloringValue = coloringInput.value
+        coloringInput.addEventListener("change", (e: Event) => {
+            const target = e.target as HTMLSelectElement
+            coloringValue = target.value
+            Render()
+        })
+    }
+    const offsetInput = document.getElementById("offset") as HTMLSelectElement | null
+    if (offsetInput !== null) {
+        offsetValue = Number(offsetInput.value)
+        offsetInput.addEventListener("input", (e: Event) => {
+            const target = e.target as HTMLSelectElement
+            offsetValue = Number(target.value)
+            const label = target.nextElementSibling
+            if (label !== null) {
+                label.textContent = String(offsetValue)
+            }
+            Render()
+        })
+    }
+}
+
+function initShaderProgram(gl: WebGLRenderingContext, vertexShaderSource: string, fragmentShaderSource: string) {
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vertexShaderSource)
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)
+
+    const shaderProgram = gl.createProgram() as WebGLShader
+    gl.attachShader(shaderProgram, vertexShader)
+    gl.attachShader(shaderProgram, fragmentShader)
+    gl.linkProgram(shaderProgram)
+
+    const success = gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)
+    if (success === false) {
+        throw new Error(`Unable to initialize the shader program: ${gl.getProgramInfoLog(shaderProgram)}`)
+    }
+
+    return shaderProgram
+}
+
+function loadShader(gl: WebGLRenderingContext, type: GLenum, source: string): WebGLShader {
+    const shader = gl.createShader(type) as WebGLShader
+
+    gl.shaderSource(shader, source)
+    gl.compileShader(shader)
+
+    const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
+    if (success === false) {
+        // gl.deleteShader(shader);
+        throw new Error(`An error occurred compiling the shaders: ${gl.getShaderInfoLog(shader)}`)
+    }
+
+    return shader
+}
+
+function initBuffers(gl: WebGLRenderingContext) {
+    const positionBuffer = initPositionBuffer(gl);
+    // const colorBuffer = initColorBuffer(gl);
+
+    return {
+        position: positionBuffer,
+        // color: colorBuffer,
+    };
+}
+
+function initPositionBuffer(gl: WebGLRenderingContext) {
+    // Create a buffer for the square's positions.
+    const positionBuffer = gl.createBuffer();
+
+    // Select the positionBuffer as the one to apply buffer
+    // operations to from here out.
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    //Two triangles covering the whole viewport
+    const positions = [
+        -1, 1, -1, -1, 1, 1,
+        1, 1, -1, -1, 1, -1
+    ]
+
+    // Now pass the list of positions into WebGL to build the
+    // shape. We do this by creating a Float32Array from the
+    // JavaScript array, then use it to fill the current buffer.
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    return positionBuffer;
+}
+
+function initColorBuffer(gl: WebGLRenderingContext) {
+    const colors = [
+        1.0,
+        1.0,
+        1.0,
+        1.0, // white
+        1.0,
+        0.0,
+        0.0,
+        1.0, // red
+        0.0,
+        1.0,
+        0.0,
+        1.0, // green
+        0.0,
+        0.0,
+        1.0,
+        1.0, // blue
+    ];
+
+    const colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+    return colorBuffer;
+}
+
+function drawScene(gl: WebGLRenderingContext, programInfo: any, buffers: any) {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0); // Opaque black
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    gl.useProgram(programInfo.program);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+    gl.vertexAttribPointer(programInfo.attribLocations.vertexPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+
+    gl.uniform3fv(programInfo.uniformLocations.resolution, [gl.canvas.width, gl.canvas.height, 1])
+    gl.uniform1f(programInfo.uniformLocations.distanceOffset, offsetValue)
+
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+}
