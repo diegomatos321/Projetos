22274267a3c09f20593cdcd9edf2fb37e4b4b1aa
diegomatos321 <diegovasconcelos62@gmail.com>
2025-03-26 18:17:49 -0300

    Movimentação dentro do labirinto

--- computacao-grafica/final-project-3d-labyrinth/src/Game.ts
+++ computacao-grafica/final-project-3d-labyrinth/src/Game.ts
@@ -1,29 +1,27 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 import Maze from './Maze';
-import { Direction } from './Cell';
+import Cell, { Direction } from './Cell';
+import Player from './Player';
 
-export default class Game
-{
+export default class Game {
     private entities: THREE.Object3D[] = [];
     private scene: THREE.Scene;
-    private camera: THREE.PerspectiveCamera;
+    private mainCamera: THREE.PerspectiveCamera;
     private renderer: THREE.WebGLRenderer;
-    private controls: OrbitControls;
 
     private maze: Maze;
+    private player: Player;
 
-    constructor()
-    {
+    constructor() {
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        // this.mainCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
         this.renderer = new THREE.WebGLRenderer({ antialias: true });
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         document.body.appendChild(this.renderer.domElement);
 
-        this.controls = new OrbitControls( this.camera, this.renderer.domElement );
+        // this.controls = new OrbitControls( this.mainCamera, this.renderer.domElement );
 
         let lightDir = new THREE.DirectionalLight(0xffffff, 1);
         lightDir.position.set(1, 1, 1);
@@ -33,76 +31,98 @@ export default class Game
         this.scene.add(lightAmb)
 
         this.maze = new Maze(10, 10);
+        this.player = new Player(this.scene);
+        this.mainCamera = this.player.camera;
+        this.entities.push(this.player);
     }
 
-    public Start()
-    {
-        this.camera.position.set(4, 5, 11);
-        // this.camera.rotateX(-Math.PI / 4);
-
+    public Start() {
         this.maze.Generate();
-        this.maze.mazeGrid.forEach(row => {
-            row.forEach(cell => {
-                const group = new THREE.Group();
-                group.position.set(cell.position.x, 0, cell.position.y);
-                
-                const floor = new THREE.PlaneGeometry();
-                const floorMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-                const floorMesh = new THREE.Mesh(floor, floorMaterial);
-                floorMesh.position.set(0, 0, 0);
-                floorMesh.rotateX(-Math.PI / 2);
+        this.maze.mazeGrid.forEach(this.RenderMaze.bind(this));
 
-                group.add(floorMesh);
-
-                cell.walls.forEach((value, key) => {
-                    if (value === false) {
-                        return
-                    }
-
-                    const wallGeometry = new THREE.PlaneGeometry();
-                    const wallMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide });
-                    const wallMesh = new THREE.Mesh(wallGeometry, wallMaterial);
-                    wallMesh.position.set(0, 0, 0);
-
-                    switch (key) {
-                        case Direction.Front:
-                            wallMesh.position.set(0, 0.5, 0.5);
-                            // wallMesh.rotateX(Math.PI / 2);
-                            break;
-                        case Direction.Backward:
-                            wallMesh.position.set(0, 0.5, -0.5);
-                            // wallMesh.rotateX(-Math.PI / 2);
-                            break;
-                        case Direction.Left:
-                            wallMesh.position.set(-0.5, 0.5, 0);
-                            wallMesh.rotateY(Math.PI / 2);
-                            break;
-                        case Direction.Right:
-                            wallMesh.position.set(0.5, 0.5, 0);
-                            wallMesh.rotateY(-Math.PI / 2);
-                            break;
-                    }
-
-                    group.add(wallMesh);
-                })
-                
-                this.scene.add(group);
-                this.entities.push(group);
-            })
-        })
+        const firstCell = this.maze.mazeGrid[0][0];
+        this.mainCamera.position.set(firstCell.position.x, .5, firstCell.position.y);
+        const firstNeighbour = firstCell.neighbours.entries().next().value;
+        if (firstNeighbour) {
+            switch (firstNeighbour[0]) {
+                case Direction.Front:
+                    break;
+                case Direction.Backward:
+                    // this.player.rotateY(-Math.PI);
+                    this.mainCamera.rotateY(-Math.PI);
+                    break;
+                case Direction.Left:
+                    // this.player.rotateY(Math.PI / 2 );
+                    this.mainCamera.rotateY(Math.PI / 2 );
+                    break;
+                case Direction.Right:
+                    // this.player.rotateY(-Math.PI / 2);
+                    this.mainCamera.rotateY(-Math.PI / 2);
+                    break;
+                default:
+                    break;
+            }
+        }
 
         this.renderer.setAnimationLoop(this.Update.bind(this));
     }
 
-    public Update()
-    {
-        /* this.entities.forEach(entity => {
-            entity.rotation.x += 0.01;
-            entity.rotation.y += 0.01;
-        }) */
+    public Update() {
+        this.entities.forEach(entity => {
+            if (entity instanceof Player) {
+                entity.Update();
+            }
+        })
+        this.renderer.render(this.scene, this.mainCamera);
+    }
 
-        this.controls.update();
+    private RenderMaze(row: Cell[]) {
+        row.forEach(cell => {
+            const group = new THREE.Group();
+            group.position.set(cell.position.x, 0, cell.position.y);
 
-        this.renderer.render(this.scene, this.camera);
+            const floor = new THREE.PlaneGeometry();
+            const floorMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+            const floorMesh = new THREE.Mesh(floor, floorMaterial);
+            floorMesh.position.set(0, 0, 0);
+            floorMesh.rotateX(-Math.PI / 2);
+
+            group.add(floorMesh);
+
+            cell.walls.forEach((value, key) => {
+                if (value === false) {
+                    return
+                }
+
+                const wallGeometry = new THREE.PlaneGeometry();
+                const wallMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide });
+                const wallMesh = new THREE.Mesh(wallGeometry, wallMaterial);
+                wallMesh.position.set(0, 0, 0);
+
+                switch (key) {
+                    case Direction.Front:
+                        wallMesh.position.set(0, 0.5, 0.5);
+                        // wallMesh.rotateX(Math.PI / 2);
+                        break;
+                    case Direction.Backward:
+                        wallMesh.position.set(0, 0.5, -0.5);
+                        // wallMesh.rotateX(-Math.PI / 2);
+                        break;
+                    case Direction.Left:
+                        wallMesh.position.set(-0.5, 0.5, 0);
+                        wallMesh.rotateY(Math.PI / 2);
+                        break;
+                    case Direction.Right:
+                        wallMesh.position.set(0.5, 0.5, 0);
+                        wallMesh.rotateY(-Math.PI / 2);
+                        break;
+                }
+
+                group.add(wallMesh);
+            })
+
+            this.scene.add(group);
+            this.entities.push(group);
+        })
     }
 }
---
+++ computacao-grafica/final-project-3d-labyrinth/src/Player.ts
@@ -0,0 +1,115 @@
+import * as THREE from 'three';
+
+export default class Player extends THREE.Object3D
+{
+    public camera: THREE.PerspectiveCamera;
+
+    private scene: THREE.Scene;
+
+    constructor(scene: THREE.Scene)
+    {
+        super();
+        
+        this.scene = scene;
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+        window.document.addEventListener('keydown', this.OnKeyDown.bind(this));
+    }
+
+    public Update()
+    {
+        this.camera.position.set(this.position.x, .5, this.position.z);
+        this.camera.rotation.set(this.rotation.x, this.rotation.y, this.rotation.z);
+    }
+
+    isAnimating = false;
+    private async OnKeyDown(event: KeyboardEvent): Promise<void> {
+        if (this.isAnimating) {
+            return;
+        }
+        
+        this.isAnimating = true;
+        switch (event.key) {
+            case 'w':
+                // this.camera.translateZ(-1);
+                await this.SmoothMovement(-1);
+                break;
+            case 's':
+                this.SmoothMovement(1);
+                break;
+            case 'a':
+                await this.SmoothRotation(Math.PI / 2);
+                break;
+            case 'd':
+                await this.SmoothRotation(-Math.PI / 2);
+                break;
+        }
+
+        this.isAnimating = false;
+    }
+
+    private async SmoothMovement(distance: number): Promise<void> {
+        const duration = 250; // Animation duration in milliseconds
+        let startTime: number | null = null;
+        const startPosition = this.position.clone(); // Clone initial position
+        
+        const direction = new THREE.Vector3();
+        this.getWorldDirection(direction); // Get forward direction
+        direction.normalize(); // Normalize direction vector
+        
+        const finalPosition = startPosition.clone().addScaledVector(direction, distance);
+
+        return new Promise((resolve, reject) => {
+            const animate = (currentTime: number): void => {
+                if (startTime === null) startTime = currentTime;
+        
+                const timeElapsed = currentTime - startTime;
+                let t = Math.min(timeElapsed / duration, 1); // Normalize t to range [0, 1]
+        
+                // Interpolate between start and final position
+                this.position.lerpVectors(startPosition, finalPosition, t);
+
+                if (t < 1) {
+                    window.requestAnimationFrame(animate);
+                } else {
+                    resolve();
+                }
+            };
+            window.requestAnimationFrame(animate);
+        })    
+    }
+
+    private async SmoothRotation(angle: number): Promise<void> {
+        const duration = 250; // Rotation duration in ms
+        let startTime: number | null = null;
+        
+        // Convert current rotation to quaternion
+        const startQuaternion = this.quaternion.clone();
+        
+        // Compute the target rotation
+        const rotationQuaternion  = new THREE.Quaternion();
+        rotationQuaternion.setFromAxisAngle(new THREE.Vector3(0, 1, 0), angle);
+        
+        // Multiply to get the correct final target rotation
+        const targetQuaternion = startQuaternion.clone().multiply(rotationQuaternion);
+        
+        return new Promise((resolve, reject) => {
+            const animate = (currentTime: number): void => {
+                if (startTime === null) startTime = currentTime;
+                const timeElapsed = currentTime - startTime;
+                let t = Math.min(timeElapsed / duration, 1); // Normalized time factor
+                
+                // Spherical interpolation between current and target rotation
+                this.quaternion.slerpQuaternions(startQuaternion, targetQuaternion, t);
+        
+                if (t < 1) {
+                    window.requestAnimationFrame(animate);
+                } else {
+                    resolve();
+                }
+            };
+            window.requestAnimationFrame(animate);
+        })
+    }
+    
+}
