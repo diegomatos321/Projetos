d533c3682f79985ef8301fcd990268b5044dd499
diegomatos321 <diegovasconcelos62@gmail.com>
2025-03-27 22:18:25 -0300

    Testando tirar fotos da cena

--- computacao-grafica/promotional-tent-product-configurator/src/js/configurator.ts
+++ computacao-grafica/promotional-tent-product-configurator/src/js/configurator.ts
@@ -12,6 +12,9 @@ export default defineComponent(() => ({
 
     scene: new THREE.Scene(),
     camera: new THREE.PerspectiveCamera(),
+    renderer: new THREE.WebGLRenderer(),
+
+    snap: false,
 
     init() {
         if (WebGL.isWebGL2Available()) {
@@ -25,25 +28,26 @@ export default defineComponent(() => ({
 
     Start() {
         const container = this.$refs.configurator;
-    
+
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.01, 1000);
-        this.camera.position.z = 1;
-    
-        const renderer = Alpine.raw(new THREE.WebGLRenderer({ antialias: true, alpha: true }));
-        renderer.setSize(container.clientWidth, container.clientHeight);
-        renderer.setPixelRatio(window.devicePixelRatio);
-        container.appendChild(renderer.domElement);
-    
+        this.camera.position.x = -1;
+
+        this.renderer = Alpine.raw(new THREE.WebGLRenderer({ antialias: true, alpha: true, preserveDrawingBuffer: true }));
+        this.renderer.autoClearColor = false;
+        this.renderer.setSize(container.clientWidth, container.clientHeight);
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        container.appendChild(this.renderer.domElement);
+
         const ambientLight = Alpine.raw(new THREE.AmbientLight(0xffffff, 1));
         this.scene.add(ambientLight);
-    
+
         const directionalLight = Alpine.raw(new THREE.DirectionalLight());
         directionalLight.position.set(0, 10, 0);
         this.scene.add(directionalLight);
-    
-        new OrbitControls(this.camera, renderer.domElement);
-    
+
+        new OrbitControls(this.camera, this.renderer.domElement);
+
         const gltfLoader = Alpine.raw(new GLTFLoader());
         gltfLoader.load("./assets/models/demo-tent/scene.gltf", (gltf) => {
             gltf.scene.traverse((child) => {
@@ -53,12 +57,18 @@ export default defineComponent(() => ({
             });
             this.scene.add(Alpine.raw(gltf.scene))
         });
-    
-        renderer.domElement.addEventListener("pointerup", this.HandlePointerDown.bind(this));
-    
-        renderer.setAnimationLoop(() => renderer.render(Alpine.raw(this.scene), Alpine.raw(this.camera)));
+
+        this.renderer.domElement.addEventListener("pointerup", this.HandlePointerDown.bind(this));
+
+        this.renderer.setAnimationLoop(() => {
+            this.renderer.render(Alpine.raw(this.scene), Alpine.raw(this.camera))
+            if (this.snap) {
+                this.ExportTexture()
+                this.snap = false
+            }
+        });
     },
-    
+
     HandlePointerDown(event: PointerEvent) {
         const parentBoundings = this.$refs.configurator.getBoundingClientRect();
         const mouse = new THREE.Vector2(
@@ -83,9 +93,14 @@ export default defineComponent(() => ({
         }
     },
 
-    ChangeSize(){
+    ChangeSize() {
         const [width, length] = this.selectedSize.split('x').map(Number);
         this.scene.getObjectByName("Tent_M_Tent_0")?.scale.set(width, 5, length);
         this.scene.getObjectByName("Tent_M_Tent_0_1")?.scale.set(width, 5, length);
+    },
+
+    ExportTexture() {
+        const exportedTexture = this.renderer.domElement.toDataURL("image/png");
+        console.log(exportedTexture); // Send to server or download
     }
 }))
