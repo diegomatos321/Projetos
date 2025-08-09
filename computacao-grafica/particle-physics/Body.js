eef17099af39ee9332263d9b1f8b47810eaeb8e8
diegomatos321 <diegovasconcelos62@gmail.com>
2025-08-09 11:53:03 -0300

    Extraindo código para classes e permitindo quebrar elos entre as particulas

---
+++ computacao-grafica/particle-physics/Body.js
@@ -0,0 +1,44 @@
+export default class Body {
+    particles = []
+    constraints = []
+    NUM_ITERATIONS = 10
+
+    constructor(gl, particles, constraints) {
+        this.gl = gl
+        this.particles = particles;
+        this.constraints = constraints;
+    }
+
+    update(dt) {
+        for (const particle of this.particles) {
+            particle.update(dt)
+        }
+
+        for (let i = 0; i < this.NUM_ITERATIONS; i++) {
+            for (const particle of this.particles) {
+                let x = Math.max(Math.min(particle.position[0], this.gl.canvas.width), 0);
+                let y = Math.max(Math.min(particle.position[1], this.gl.canvas.height), 0);
+                particle.move(twgl.v3.create(x, y));
+
+                // particle.move(twgl.v3.create(
+                //     particle.position[0] * (1 - 0.5) + x * 0.5,
+                //     particle.position[1] * (1 - 0.5) + y * 0.5,
+                // ))
+            }
+
+            for (const constraint of this.constraints) {
+                constraint.relax();
+            }
+        }
+    }
+
+    draw() {
+        for (const particle of this.particles) {
+            particle.draw();
+        }
+
+        for (const constraint of this.constraints) {
+            constraint.draw();
+        }
+    }
+}
---
+++ computacao-grafica/particle-physics/Particle.js
@@ -0,0 +1,52 @@
+export default class Particle {
+    mass = 1;
+    pinned = false;
+    gravity = twgl.v3.create(0, 98, 0);
+
+    constructor(gl, position = twgl.v3.create()) {
+        this.gl = gl
+        this.position = position;
+        this.previousPosition = twgl.v3.copy(position);
+
+        this.bufferInfo = twgl.createBufferInfoFromArrays(gl, {
+            a_position: {
+                numComponents: 2,
+                data: [position[0], position[1]],
+                drawType: gl.DYNAMIC_DRAW
+            },
+        });
+
+        this.programInfo = twgl.createProgramInfo(gl, ["vs", "fs"]);
+    }
+
+    update(dt) {
+        if (this.pinned) return;
+
+        let tmp = twgl.v3.copy(this.position);
+        this.position[0] = 2 * this.position[0] - this.previousPosition[0]
+        this.position[1] = 2 * this.position[1] - this.previousPosition[1] + this.gravity[1] * Math.pow(dt, 2)
+        this.previousPosition = tmp;
+    }
+
+    move(newPosition) {
+        if (this.pinned) return
+
+        this.position[0] = newPosition[0];
+        this.position[1] = newPosition[1];
+    }
+
+    draw() {
+        const uniforms = {
+            u_resolution: [this.gl.canvas.width, this.gl.canvas.height],
+            u_color: [1, 0.2, 0.2, 1]
+            // time: time * 0.001,
+        };
+
+        this.gl.useProgram(this.programInfo.program);
+        twgl.setAttribInfoBufferFromArray(this.gl, this.bufferInfo.attribs.a_position, [this.position[0], this.position[1]]);
+
+        twgl.setBuffersAndAttributes(this.gl, this.programInfo, this.bufferInfo);
+        twgl.setUniforms(this.programInfo, uniforms);
+        twgl.drawBufferInfo(this.gl, this.bufferInfo, this.gl.POINTS);
+    }
+}
